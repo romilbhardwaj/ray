40680886dfd3d50a75f119cbfff8bb26f186e67e
Romil <romil.bhardwaj@gmail.com>
2019-01-14 16:16:02 -0800

    Move create_resource and delete_resource to ray.experimental.

--- python/ray/experimental/__init__.py
+++ python/ray/experimental/__init__.py
@@ -11,6 +11,7 @@ from .gcs_flush_policy import (set_flushing_policy, GcsFlushPolicy,
                                SimpleGcsFlushPolicy)
 from .named_actors import get_actor, register_actor
 from .api import get, wait
+from .dynamic_resources import create_resource, delete_resource
 
 __all__ = [
     "TensorFlowVariables", "flush_redis_unsafe",
@@ -18,5 +19,5 @@ __all__ = [
     "flush_evicted_objects_unsafe", "_flush_finished_tasks_unsafe_shard",
     "_flush_evicted_objects_unsafe_shard", "get_actor", "register_actor",
     "get", "wait", "set_flushing_policy", "GcsFlushPolicy",
-    "SimpleGcsFlushPolicy"
+    "SimpleGcsFlushPolicy", "create_resource", "delete_resource"
 ]
